c6b0242c5dc54d006f2856c30cd158bc252e8380
CakeCrusher <37946988+CakeCrusher@users.noreply.github.com>
2020-04-30 13:51:44 -0400

    fixed jwt

--- resolvers/mutations.js
+++ resolvers/mutations.js
@@ -4,6 +4,7 @@ const Post = require('../models/post')
 const Skill = require('../models/skill')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const JWT_SECRET = process.env.JWT_SECRET
 const { UserInputError, AuthenticationError } = require('apollo-server-express')
 
 module.exports = {
