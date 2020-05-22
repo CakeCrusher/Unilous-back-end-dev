b08fcc30465c26d54d5e4688f1dfc2081f482d28
Victor Ray <victor.ray22@gmail.com>
2020-05-22 16:34:31 +0530

    Update index.js file

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
   // Exprees will serve up production assets
-  app.use(express.static("client/build"));
+  app.use(express.static("temp_build/static"));
 
   // Express serve up index.html file if it doesn't recognize route
   const path = require("path");
