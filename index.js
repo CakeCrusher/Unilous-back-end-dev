9f4c69f9d812ac97f8068c096a726493d6aec31a
Victor Ray <victor.ray22@gmail.com>
2020-05-30 12:52:54 +0530

    Upadte JWT_SECRET

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require("dotenv").config({ path: ".env-dev-pg" });
 const express = require("express");
 //const mongoose = require('mongoose')
 const User = require("./models/user");
@@ -14,7 +14,7 @@ const { gql, ApolloServer } = require("apollo-server-express");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
 const JWT_SECRET = process.env.JWT_SECRET;
-const MONGODB_URI = process.env.MONGODB_URI;
+//const MONGODB_URI = process.env.MONGODB_URI;
 const port = process.env.PORT || 4000;
 
 // mongoose.connect(MONGODB_URI, {useNewUrlParser: true})
@@ -36,26 +36,26 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
   introspection: true,
-  playground: true,
-  context: async ({ req }) => {
-    const auth = req ? req.headers.authorization : null;
+  playground: true
+  // context: async ({ req }) => {
+  //   const auth = req ? req.headers.authorization : null;
 
-    if (auth && auth.startsWith(`${JWT_SECRET} `)) {
-      const token = jwt.verify(
-        auth.substring(JWT_SECRET.length + 1),
-        JWT_SECRET
-      );
-      const currentUser = await User.findById(token._id).populate([
-        "primarySkills",
-        "secondarySkills",
-        "posts",
-        "notifications",
-        "savedPosts",
-      ]);
-      return { currentUser };
-    } else {
-      return { currentUser: null };
-    }
+  //   if (auth && auth.startsWith(`${JWT_SECRET} `)) {
+  //     const token = jwt.verify(
+  //       auth.substring(JWT_SECRET.length + 1),
+  //       JWT_SECRET
+  //     );
+  //     const currentUser = await User.findById(token._id).populate([
+  //       "primarySkills",
+  //       "secondarySkills",
+  //       "posts",
+  //       "notifications",
+  //       "savedPosts",
+  //     ]);
+  //     return { currentUser };
+  //   } else {
+  //     return { currentUser: null };
+  //   }
   },
 });
 
